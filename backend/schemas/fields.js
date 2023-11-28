7872a751211d37ba1c0e176a676f085a728e9f79
Chiranjeeb Sahoo <sahoo.chiranjeeb@tftus.com>
2023-11-28 13:14:39 +0530

    updated Fields schema

--- backend/schemas/fields.js
+++ backend/schemas/fields.js
@@ -1,68 +1,142 @@
 const mongoose = require("mongoose");
 
-const fieldSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
+const fieldSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    variable: {
+      type: String,
+      required: true,
+    },
+    type: {
+      type: String,
+      enum: [
+        "text",
+        "radio",
+        "checkbox",
+        "dropdown",
+        "date",
+        "toggle",
+        "multiSelect",
+        "slider",
+      ],
+      required: true,
+    },
+    enabled: {
+      type: Boolean,
+      required: true,
+    },
+    required: {
+      type: Boolean,
+      required: true,
+    },
+    maxLength: {
+      type: Number,
+      default: 0,
+    },
+    minLength: {
+      type: Number,
+      default: 0,
+    },
+    description: {
+      type: String,
+      default: "",
+    },
+    radioOptions: {
+      type: [String],
+      default: [],
+      validate: {
+        validator: function (value) {
+          return this.type !== "radio" || value.length > 0;
+        },
+        message: 'radioOptions must be present when type is "radio"',
+      },
+    },
+    checkboxLabel: {
+      type: String,
+      default: "",
+      validate: {
+        validator: function (value) {
+          return this.type !== "checkbox" || value.trim().length > 0;
+        },
+        message: 'checkboxLabel must be present when type is "checkbox"',
+      },
+    },
+    checkboxDefault: {
+      type: Boolean,
+      default: false,
+    },
+    dropdownOptions: {
+      type: [String],
+      default: [],
+      validate: {
+        validator: function (value) {
+          return this.type !== "dropdown" || value.length > 0;
+        },
+        message: 'dropdownOptions must be present when type is "dropdown"',
+      },
+    },
+    dateOptions: {
+      format: {
+        type: String,
+        enum: ["YYYY-MM-DD", "DD-MM-YYYY"],
+        required: function () {
+          return this.type === "date";
+        },
+      },
+      minDate: {
+        type: Date,
+        required: function () {
+          return this.type === "date";
+        },
+      },
+      maxDate: {
+        type: Date,
+        required: function () {
+          return this.type === "date";
+        },
+      },
+    },
+    toggleDefault: {
+      type: Boolean,
+      default: false,
+    },
+    multiSelectOptions: {
+      type: [String],
+      default: [],
+      validate: {
+        validator: function (value) {
+          return this.type !== "multiSelect" || value.length > 0;
+        },
+        message:
+          'multiSelectOptions must be present when type is "multiSelect"',
+      },
+    },
+    sliderOptions: {
+      min: {
+        type: Number,
+        required: function () {
+          return this.type === "slider";
+        },
+      },
+      max: {
+        type: Number,
+        required: function () {
+          return this.type === "slider";
+        },
+      },
+      step: {
+        type: Number,
+        required: function () {
+          return this.type === "slider";
+        },
+      },
+    },
   },
-  variable: {
-    type: String,
-    required: true,
-  },
-  type: {
-    type: String,
-    enum: ["text", "radio"],
-    required: true,
-  },
-  enabled: {
-    type: Boolean,
-    required: true,
-  },
-  required: {
-    type: Boolean,
-    required: true,
-  },
-  default: {
-    type: String,
-    default: "",
-  },
-  dateDefault: {
-    type: Date,
-    default: "2021-07-18T16:05:42.395Z",
-  },
-  numberDefault: {
-    type: Number,
-    default: 0,
-  },
-  booleanDefault: {
-    type: Boolean,
-    default: false,
-  },
-  maxLength: {
-    type: Number,
-    default: 0,
-  },
-  minLength: {
-    type: Number,
-    default: 0,
-  },
-  description: {
-    type: String,
-    default: "",
-  },
-  radioOptions: {
-    type: [String],
-    default: [],
-  },
-  selectOptions: {
-    type: [String], // need updation
-    default: [],
-  },
-  multiSelectOptions: {
-    type: [String], // need updation
-    default: [],
-  },
-});
+  { versionKey: false, timestamps: true }
+);
 
 const Fields = mongoose.model("fields", fieldSchema);
 
