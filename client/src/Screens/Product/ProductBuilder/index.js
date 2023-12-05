f8ac4e9eec1a69c2ee95aa882aaeace65bd49041
atulsharmaTFT <94036900+atulsharmaTFT@users.noreply.github.com>
2023-12-05 17:01:53 +0530

    Multi Select DropDown

--- client/src/Screens/Product/ProductBuilder/index.js
+++ client/src/Screens/Product/ProductBuilder/index.js
@@ -3,142 +3,172 @@ import styles from "./ProductBuilder.module.scss";
 import RadioButton from "../../../components/RadioButton";
 import InputField from "../../../components/InputField";
 import DateTimePicker from "../../../components/DatePicker/DateTimePicker";
+import MultiselectDropdown from "../../../components/MultiSelectDropdown/MultiselectDropdown";
+import CheckBox from "../../../components/CheckBox/CheckBox";
+
 const ProductBuilder = ({ fields }) => {
   const [selectedDateTime, setSelectedDateTime] = useState("");
+  const [selectedOptions, setSelectedOptions] = useState([]);
+  const [dropDownOptions, setDropDownOptions] = useState([]);
+  // console.log(fields);
+  // console.log(selectedOptions,"selectedOptions")
 
-  console.log(fields);
+  const handleRadioChange = (e) => {
+    console.log(e.target.value);
+  };
+  const handleInputChange = (e) => {
+    console.log(e.target.value);
+  };
+  const handleOptionChange = (options, action, type) => {
+    if (type === "dropdown") {
+      console.log(options, action, "DropDown");
+      setDropDownOptions(options);
+    }
+    if (type === "multiSelect") {
+      console.log(options, action, "MultiSelect");
+      if (action.action === "clear") {
+        setSelectedOptions([]);
+      }
+      if (action.action === "select-option") {
+        setSelectedOptions(options);
+      }
+      if (action.action === "remove-value") {
+        const filterOption = selectedOptions.filter(
+          (elem) => elem?.value !== action?.removedValue?.value
+        );
+        setSelectedOptions(filterOption);
+      }
+    }
+  };
+  const handleCheckBoxClick = (e) => {
+    console.log(e.target.value, "check");
+  };
+  const renderField = (field) => {
+    switch (field.type) {
+      case "radio":
+        return (
+          <div className={styles.flex}>
+            {field.radioOptions.map((option, index) => {
+              return (
+                <RadioButton
+                  key={option.option}
+                  label={option.option}
+                  value={option.option}
+                  checked={option.checked}
+                  name={field.variable}
+                  onChange={handleRadioChange}
+                />
+              );
+            })}
+          </div>
+        );
+      case "checkbox":
+        return (
+          <div>
+            {field.checkboxOptions.map((option, index) => (
+              <CheckBox
+                value={option?.option}
+                title={option?.option}
+                isChecked={option?.checked}
+                onChange={handleCheckBoxClick}
+              />
+            ))}
+          </div>
+        );
+      case "multiSelect":
+        return (
+          <MultiselectDropdown
+            isMulti={true}
+            category={field?.placeholder}
+            data={field?.dropdownOptions}
+            handleChange={(selectedValue, action) =>
+              handleOptionChange(selectedValue, action, field?.type)
+            }
+            selected={selectedOptions}
+            className={styles.inputOverride}
+          />
+        );
+      case "slider":
+        return (
+          <InputField
+            key={field._id}
+            type="range"
+            fieldName={field.name}
+            placeholder={field.placeholder}
+            onChange={handleInputChange}
+            inputOverrideClassName={styles.inputOverride}
+            overrideErrorClassName={styles.overrideErrorClass}
+            containerOverrideClassName={styles.inputContainer}
+            min={field.sliderOptions.min}
+            max={field.sliderOptions.max}
+            step={field.sliderOptions.step}
+          />
+        );
+      case "dropdown":
+        return (
+          <MultiselectDropdown
+            isMulti={false}
+            category={field?.placeholder}
+            data={field?.dropdownOptions}
+            handleChange={(selectedValue, action) =>
+              handleOptionChange(selectedValue, action, field?.type)
+            }
+            selected={dropDownOptions?.[0]}
+            className={styles.inputOverride}
+          />
+        );
+      case "date":
+        return (
+          <DateTimePicker
+            type="date"
+            selected={selectedDateTime}
+            setDateTime={setSelectedDateTime}
+            inputOverrideClassName={styles.inputContainer}
+            overrideClassName={styles.inputOverride}
+          />
+        );
+      case "number":
+        return (
+          <InputField
+            key={field._id}
+            type="number"
+            fieldName={field.name}
+            placeholder={field.placeholder}
+            onChange={handleInputChange}
+            inputOverrideClassName={styles.inputOverride}
+            overrideErrorClassName={styles.overrideErrorClass}
+            containerOverrideClassName={styles.inputContainer}
+          />
+        );
+      case "text":
+        return (
+          <InputField
+            type="text"
+            key={field._id}
+            fieldName={field.name}
+            placeholder={field.placeholder}
+            onChange={handleInputChange}
+            inputOverrideClassName={styles.inputOverride}
+            overrideErrorClassName={styles.overrideErrorClass}
+            containerOverrideClassName={styles.inputContainer}
+          />
+        );
+      default:
+        return null;
+    }
+  };
+
   return (
     <div className={styles["product-builder"]}>
       <h2>{fields.name}</h2>
       {fields.fields.map((field) => (
         <div className={styles.container} key={field._id}>
           <label>{field.name}:</label>
-          {renderField(field, selectedDateTime, setSelectedDateTime)}
+          {renderField(field)}
         </div>
       ))}
     </div>
   );
-};
-
-const handleRadioChange = (e) => {
-  console.log(e.target.value);
-};
-const handleInputChange = (e) => {
-  console.log(e.target.value);
-};
-
-const renderField = (field, selectedDateTime, setSelectedDateTime) => {
-  switch (field.type) {
-    case "radio":
-      return (
-        <div className={styles.flex}>
-          {field.radioOptions.map((option, index) => {
-            // {console.log(option)}
-            return (
-              <RadioButton
-                key={option.option}
-                label={option.option}
-                value={option.option}
-                checked={option.checked}
-                name={field.variable}
-                onChange={handleRadioChange}
-              />
-            );
-          })}
-        </div>
-      );
-    case "checkbox":
-      return (
-        <div>
-          {field.checkboxOptions.map((option, index) => (
-            <label key={index}>
-              <input type="checkbox" name={field.variable} value={option} />
-              {option}
-            </label>
-          ))}
-        </div>
-      );
-    case "multiSelect":
-      return (
-        <select multiple>
-          {field.multiSelectOptions.map((option, index) => (
-            <option key={index} value={option}>
-              {option}
-            </option>
-          ))}
-        </select>
-      );
-    case "slider":
-      return (
-        // <input
-        //   type="range"
-        // min={field.sliderOptions.min}
-        // max={field.sliderOptions.max}
-        // step={field.sliderOptions.step}
-        // />
-        <InputField
-          key={field._id}
-          type="range"
-          fieldName={field.name}
-          placeholder={field.placeholder}
-          onChange={handleInputChange}
-          inputOverrideClassName={styles.inputOverride}
-          overrideErrorClassName={styles.overrideErrorClass}
-          containerOverrideClassName={styles.inputContainer}
-          min={field.sliderOptions.min}
-          max={field.sliderOptions.max}
-          step={field.sliderOptions.step}
-        />
-      );
-    case "dropdown":
-      return (
-        <select>
-          {field.dropdownOptions.map((option, index) => (
-            <option key={index} value={option}>
-              {option}
-            </option>
-          ))}
-        </select>
-      );
-    case "date":
-      // return <input type="date" />;
-      return (
-        <DateTimePicker
-          type="date"
-          selected={selectedDateTime}
-          setDateTime={setSelectedDateTime}
-        />
-      );
-    case "number":
-      return (
-        <InputField
-          key={field._id}
-          type="number"
-          fieldName={field.name}
-          placeholder={field.placeholder}
-          onChange={handleInputChange}
-          inputOverrideClassName={styles.inputOverride}
-          overrideErrorClassName={styles.overrideErrorClass}
-          containerOverrideClassName={styles.inputContainer}
-        />
-      );
-    case "text":
-      return (
-        <InputField
-          type="text"
-          key={field._id}
-          fieldName={field.name}
-          placeholder={field.placeholder}
-          onChange={handleInputChange}
-          inputOverrideClassName={styles.inputOverride}
-          overrideErrorClassName={styles.overrideErrorClass}
-          containerOverrideClassName={styles.inputContainer}
-        />
-      );
-    // Add cases for other field types as needed
-    default:
-      return null;
-  }
 };
 
 export default ProductBuilder;
