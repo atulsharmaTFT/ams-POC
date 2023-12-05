029bde18ea10f010afe8a7e63087070691802349
atulsharmaTFT <94036900+atulsharmaTFT@users.noreply.github.com>
2023-12-05 21:57:52 +0530

    Revert changes

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ mongoose
 
 const options1 = Joi.object({
   option: Joi.string().min(1).required(),
-  checked: Joi.boolean().required(),
+  checked: Joi.boolean().valid(false).required(),
 });
 
 const options2 = Joi.object({
