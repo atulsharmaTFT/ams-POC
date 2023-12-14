160031d3589caa9ef29a294bbfc67fc20c8cbb03
Sanjay Kumar Tiwari <tiwari.sanjay@tftus.com>
2023-12-14 18:52:30 +0530

    Added contants folder in helper !

---
+++ client/src/helper/constantKeyword/constants.js
@@ -0,0 +1,29 @@
+// constants.js
+
+const constants = {
+    text: 'text',
+    placeholder: 'placeholder',
+    minLength: 'minLength',
+    maxLength: 'maxLength',
+    number: 'number',
+    email: 'email',
+    phone: 'phone',
+    dropdown: 'dropdown',
+    checkbox: 'checkbox',
+    radio: 'radio',
+    multiselect: 'multiselect',
+    date: 'date',
+    tagNumber: 'tagNumber',
+    name: 'name',
+    price: 'price',
+    purchaseDate: 'purchaseDate',
+    image: 'image',
+    file: 'file',
+    string: 'string',
+    boolean: 'boolean',
+    isRequired: 'isRequired',
+    isPositive: 'isPositive',
+  };
+  
+  export default constants;
+  
