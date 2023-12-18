9b9b3ccf632a1123fb0551359b91bda9d1119fde
Sanjay Kumar Tiwari <tiwari.sanjay@tftus.com>
2023-12-18 13:50:19 +0530

    add some constants keyword

---
+++ client/src/helper/constantKeyword/messages.js
@@ -0,0 +1,19 @@
+// messages.js
+
+const messages = {
+    pleaseSelect: 'Please select at least one option',
+    nameRequired: 'Name is required',
+    priceRequired: 'Price is required',
+    priceGreaterThanZero: 'Price must be greater than 0!',
+    tagNumberRequired: 'Tag number is required',
+    dateRequired: 'Date is required',
+    nameAtleast: 'Name must be at least 2 characters',
+    tagAtleast: 'Tag number must be at least 2 characters',
+    nameAtmost: 'Name must be at most 20 characters',
+    tagAtmost: 'Tag number must be at most 15 characters',
+    priceMustBeNUmber: 'Price must be a `number` type, but the final value was: `NaN` (cast from the value `""`).',
+    errorFetchingData: 'Error fetching data:'
+  };
+  
+  export default messages;
+  
