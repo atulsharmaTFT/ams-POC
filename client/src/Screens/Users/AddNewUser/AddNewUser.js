66bf2f2d0777bf09775952dc45eb338e8b960c3e
Vasav Prashar <prashar.vasav@tftus.com>
2023-12-26 11:25:15 +0530

    add new user Form

--- client/src/Screens/Users/AddNewUser/AddNewUser.js
+++ client/src/Screens/Users/AddNewUser/AddNewUser.js
@@ -1,8 +1,71 @@
 import React from 'react'
+import { useForm } from 'react-hook-form';
+import classes from "./AddNewUser.module.scss"
 
 function AddNewUser() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm();
+
+  const onSubmit=(data)=>{
+    console.log('data', data)
+    reset();
+  }
   return (
-    <div>AddNewUser</div>
+    <>
+    
+    <form className={classes.employeeForm} onSubmit={handleSubmit(onSubmit)}>
+      <label>
+        Employee ID:
+        <input type="text" {...register('employeeId', { required: 'This field is required' })} />
+        {errors.employeeId && <p>{errors.employeeId.message}</p>}
+      </label>
+
+      <label>
+        Name:
+        <input type="text" {...register('name', { required: 'This field is required' })} />
+        {errors.name && <p>{errors.name.message}</p>}
+      </label>
+
+      <label>
+        Email:
+        <input
+          type="email"
+          {...register('email', { required: 'This field is required', pattern: /^\S+@\S+$/i })}
+        />
+        {errors.email && <p>{errors.email.message}</p>}
+      </label>
+
+      <label>
+        Manager Name:
+        <input type="text" {...register('managerName', { required: 'This field is required' })} />
+        {errors.managerName && <p>{errors.managerName.message}</p>}
+      </label>
+
+      <label>
+        Designation:
+        <input type="text" {...register('designation', { required: 'This field is required' })} />
+        {errors.designation && <p>{errors.designation.message}</p>}
+      </label>
+
+      <label>
+        Department:
+        <input type="text" {...register('department')} />
+        {errors.department && <p>{errors.department.message}</p>}
+      </label>
+
+      <label>
+        Joining Date:
+        <input type="date" {...register('joiningDate')} />
+        {errors.joiningDate && <p>{errors.joiningDate.message}</p>}
+      </label>
+
+      <button type="submit">Add Employee</button>
+    </form>
+    </>
   )
 }
 
